75fb04d220e2ed433e5ce049e508ef10a1cae58d
Brett Wilson <brettwilson7@yahoo.com>
2016-03-01 00:17:20 -0600

    board settings with css

--- public/javascripts/lettersController.js
+++ public/javascripts/lettersController.js
@@ -2,13 +2,17 @@
 
 	var app = angular.module('BeforeAndAfterApp');
 
-	app.controller('LetterController', function(){
+	app.controller('LettersController', function(){
 		
+		this.setloop = function(len){
+			return this.arr = new Array(len);
+		};
+    	
 		//get data from database somehow
 			//select a phrase by random
 			//split the characters in that string into an array
 			//name the array so ng-repeat can iterate over it and create many elements on the page
-			return this;
+			//return this;
 	});
 
 })();
